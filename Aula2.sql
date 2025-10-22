57d64f4058931b8146c86cbbe1c10fa272ae7fa2
Luiz Henrique <luizhenrique3781@gmail.com>
2025-10-21 21:39:13 -0300

    Add files via upload

---
+++ Aula2.sql
@@ -0,0 +1,32 @@
+INSERT INTO usuarios (id, nome, email, senha, celular, cpf, criado_em)
+VALUES
+(1, 'Breno Garcia', 'alexia96@example.net','td2Zp35g4!', '(084) 8081-6924', '817.432.695-28', 	NOW());
+
+INSERT INTO usuarios(id, nome, email, senha, celular, cpf, criado_em)
+VALUES
+(2, 'Liam Barbosa', 'aragamaite@example.net', ')b82qDFCh1', '+55 31 6638-5493', '062.791.835-21', NOW());
+
+INSERT INTO usuarios(id, nome, email, senha, celular, cpf, criado_em)
+VALUES
+(3, 'Brayan Ferreira', 'dsousa@example.com', 'noR9bDVpM(', '71 7271 5411', '563.871.204-62', NOW());
+
+INSERT INTO usuarios(id, nome, email, senha, celular, cpf, criado_em)
+VALUES
+(4, 'Arthur Miguel Montenegro', 'luiz-otaviofreitas@example.net', '%I5ypQxQpw', '+55 11 9593 1624', '508.142.937-60', NOW());
+
+INSERT INTO usuarios(id, nome, email, senha, celular, cpf, criado_em)
+VALUES
+(5, 'Cauã Pastor', 'ana-ceciliafarias@example.org', '@Dk12Wca_J', '0500-051-8206', '706.459.182-02', NOW());
+
+INSERT INTO usuarios(id, nome, email, senha, celular, cpf, criado_em)
+VALUES
+(6, 'Sophia Albuquerque', 'apollofonseca@example.net', 'D´5K88WpiF', '(031) 6016-5695', '745.126.389-46', NOW());
+
+INSERT INTO usuarios(id, nome,  email, senha, celular, cpf, criado_em)
+VALUES
+(7, 'Stella Nascimento', 'henry-gabrielmonteiro@example.org', 'p7_D@5qb_V', '+55 (081) 2803 7473', '526.431.780-17', NOW());
+
+INSERT INTO usuarios(id, nome, email, senha, celular, cpf, criado_em)
+VALUES
+(8, 'João Guilherme Monteiro', 'rhavialmeida@example.com', 'm`U#zHEN$1', '(084) 7065 8106', '492.018.753-04', NOW());
+
