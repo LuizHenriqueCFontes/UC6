fc69d8745cf50ef9cd96ed358a538c73f0672a34
Luiz Henrique <luizhenrique3781@gmail.com>
2025-11-19 20:34:54 -0300

    Add files via upload

---
+++ Procedures.sql
@@ -0,0 +1,35 @@
+DELIMITER //
+
+CREATE PROCEDURE listar_produtos_por_categoria(IN categoria_id_param INT)
+
+BEGIN
+	SELECT p.nome,
+		   p.preco,
+           p.estoque,
+           c.nome AS categoria
+	FROM produtos AS p
+	JOIN categorias AS c ON c.id = p.categoria_id
+    WHERE p.categoria_id = categoria_id_param;
+
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE atualizar_preco_categoria(
+	IN categoria_id_param INT,
+    IN novo_preco DECIMAL(10, 2)
+)
+
+BEGIN
+	UPDATE produtos
+    SET preco = novo_preco
+    WHERE categoria_id = categoria_id_param;
+    
+END//
+
+DELIMITER ;
+
+CALL atualizar_preco_categoria(1, 456.00);
+
